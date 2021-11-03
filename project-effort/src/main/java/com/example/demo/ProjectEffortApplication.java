9fbd1c5d43906f149d719d8f2f31c23915121d3a
mschwa1010 <80753072+mschwa1010@users.noreply.github.com>
2021-11-03 12:25:59 +0100

    Projektrahmen erzeugt.

---
+++ project-effort/src/main/java/com/example/demo/ProjectEffortApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEffortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEffortApplication.class, args);
+	}
+
+}
